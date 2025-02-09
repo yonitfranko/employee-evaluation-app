c41a5fbd0184a06892ab1fb384cb22560f8b195e
yonitfranko <yonitfranko@gmail.com>
2025-02-09 19:16:46 +0200

    Update button.jsx

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground shadow hover:bg-primary/90",
-        outline: "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+      outline: "border border-pink-300 bg-white shadow-sm hover:bg-pink-50", 
       },
       size: {
         default: "h-9 px-4 py-2",
